6f24f1b725e2c3a333651ddacc544568f03a3b5a
KeerthiSaiPG <147936219+KeerthiSaiPG@users.noreply.github.com>
2025-03-27 07:27:37 +0530

    Create quick sort.cpp

---
+++ medium/quick sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+int f(vector<int>& arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i<j){
+        while(arr[i]<=pivot && i<=high-1) i++;
+        while(arr[j]>pivot && j>=low+1) j--;
+        if(i<j) swap(arr[i],arr[j]); 
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void qs(vector<int>& arr, int low, int high){
+    if(low<high){
+        int partition = f(arr,low,high);
+        qs(arr,low,partition-1);
+        qs(arr,partition+1,high);
+    }
+}
+vector<int> quickSort(vector<int> arr)
+{
+    // Write your code here.
+    qs(arr,0,arr.size()-1);
+    return arr;
+}
