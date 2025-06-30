f37d8a2481cdf960b49a22a4975a6d2df2622e3c
KeerthiSaiPG <147936219+KeerthiSaiPG@users.noreply.github.com>
2025-06-30 16:24:40 +0530

    Update Longest increasing subsequence.cpp

--- medium/Longest increasing subsequence.cpp
+++ medium/Longest increasing subsequence.cpp
@@ -1,3 +1,4 @@
+//memoization
 class Solution {
 public:
     int f(int ind, int prevInd,int n, vector<int>& nums, vector<vector<int>>& dp){
@@ -16,5 +17,26 @@ public:
         //co-ordinal change as we cannot store -1 (prevInd starts with -1)
         vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
         return f(0,-1,n,nums,dp);
+    }
+};
+
+//tabulation
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prevInd=ind-1;prevInd>=-1;prevInd--){
+                //not take 
+                int len = 0 + dp[ind+1][prevInd+1];
+                //take 
+                if(prevInd==-1 || nums[ind]>nums[prevInd]){
+                    len = max(len,1 + dp[ind+1][ind+1]);
+                }
+                dp[ind][prevInd+1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
