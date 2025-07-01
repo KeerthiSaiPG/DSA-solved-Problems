15cb5171068f721c30ce1f1ab70ae3363aa7eab9
KeerthiSaiPG <147936219+KeerthiSaiPG@users.noreply.github.com>
2025-07-01 21:15:26 +0530

    Update Longest increasing subsequence.cpp

--- medium/Longest increasing subsequence.cpp
+++ medium/Longest increasing subsequence.cpp
@@ -40,3 +40,21 @@ public:
         return dp[0][0];
     }
 };
+//alternative solution (useful for printing lis)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1);
+        int maxi = 1;
+        for(int ind=0;ind<n;ind++){
+            for(int prev=0;prev<=ind-1;prev++){
+                if(nums[ind]>nums[prev]){
+                    dp[ind] = max(1+dp[prev], dp[ind]);
+                }
+            }
+            maxi = max(maxi,dp[ind]);
+        }
+        return maxi;
+    }
+};
